fdb7e671cc9235d6fe5dbe187e9a8fe203af4b77
Graham Deubner <gdeubner@gmail.com>
2020-09-25 09:40:24 -0400

    Revert "Gdeubner"

--- Project1_clean/Javadocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"shoppingProject","l":"GroceryItem"},{"p":"shoppingProject","l":"RunProject1"},{"p":"shoppingProject","l":"Shopping"},{"p":"shoppingProject","l":"ShoppingBag"}]
